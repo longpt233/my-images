b821a4a074e1e3319423b56ba5c76f7c3da9ceb2
longpt <longphanthanh@tech.admicro.vn>
2022-05-12 17:05:06 +0700

    add streaming

---
+++ 008_flink/flink-test/src/main/java/com/company/team/main/WikipediaAnalysis.java
@@ -0,0 +1,66 @@
+package com.company.team.main;
+
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
+import scala.Tuple2;
+
+public class WikipediaAnalysis {
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<WikipediaEditEvent> edits = env.addSource(new WikipediaEditsSource());
+        /*
+        1. timestamp
+        2. channel
+        3. title
+        4. diffUrl
+        5. user
+        6. byteDiff
+        7. summary
+        8. flags
+         */
+
+        KeyedStream<WikipediaEditEvent, String> keyedEdits = edits
+                .keyBy((KeySelector<WikipediaEditEvent, String>) WikipediaEditEvent::getUser);
+
+        DataStream<Tuple2<String, Long>> result = keyedEdits
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
+                .aggregate(new AggregateFunction<WikipediaEditEvent, Tuple2<String, Long> , Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> createAccumulator() {
+                        return new Tuple2<>("", 0L);
+                    }
+
+                    @Override
+                    public Tuple2<String, Long> add(WikipediaEditEvent value, Tuple2<String, Long> accumulator) {
+                        String log = String.format("%s\t%40s\t%d",value.getTimestamp(),value.getUser(),value.getByteDiff());
+                        System.out.println(log);
+                        Long totalDiff = value.getByteDiff() + accumulator._2;
+                        Tuple2<String, Long> acc = new Tuple2<>(value.getUser(), totalDiff);
+                        return acc;
+                    }
+
+                    @Override
+                    public Tuple2<String, Long> getResult(Tuple2<String, Long> accumulator) {  // print cho 1 cai window
+                        System.out.println(accumulator);
+                        return accumulator;
+                    }
+
+                    @Override
+                    public Tuple2<String, Long> merge(Tuple2<String, Long> acc1, Tuple2<String, Long> acc2) {
+                        return new Tuple2<>(acc1._1, acc1._2 + acc2._2);
+                    }
+                });
+
+        env.execute();
+    }
+}
